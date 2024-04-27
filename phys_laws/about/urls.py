84289cfc72d873176dc327813d0069049a986851
eugenii <eugenii.cher@gmail.com>
2024-04-27 16:48:25 +0300

    first commit

--- phys_laws/about/urls.py
+++ phys_laws/about/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.law_list),
+    path('', views.about),
 ]
--- phys_laws/homepage/views.py
+++ phys_laws/homepage/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
+
 def index(request):
     template_name = 'homepage/index.html'
+    return render(request, template_name)
+
+
+def about(request):
+    template_name = 'about/about.html'
     return render(request, template_name)
 
